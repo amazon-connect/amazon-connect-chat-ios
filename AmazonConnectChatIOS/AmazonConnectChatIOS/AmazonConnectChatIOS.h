9f69785788d5473d26f953e82074bd634f077636
Rajat Mittal <rajatttt@amazon.com>
2024-03-28 13:06:18 -0700

    skeleton

---
+++ AmazonConnectChatIOS/AmazonConnectChatIOS/AmazonConnectChatIOS.h
@@ -0,0 +1,18 @@
+//
+//  AmazonConnectChatIOS.h
+//  AmazonConnectChatIOS
+//
+//  Created by Mittal, Rajat on 3/28/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AmazonConnectChatIOS.
+FOUNDATION_EXPORT double AmazonConnectChatIOSVersionNumber;
+
+//! Project version string for AmazonConnectChatIOS.
+FOUNDATION_EXPORT const unsigned char AmazonConnectChatIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmazonConnectChatIOS/PublicHeader.h>
+
+
