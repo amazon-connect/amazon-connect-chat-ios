fa0a6eeb201f08d61d1c1b7ad8534a4b10234fe6
mrajatttt <143978428+mrajatttt@users.noreply.github.com>
2024-07-11 14:11:49 -0700

    Delete AmazonConnectChatIOS/build-release directory (#29)

--- AmazonConnectChatIOS/build-release/1.0.8/Build/Intermediates.noindex/AWSiOSSDKV2.build/Release-iphoneos/AWSConnectParticipant-Target.build/Objects-normal/arm64/empty.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/rajatttt/Mobile/release/amazon-connect-chat-ios/AmazonConnectChatIOS/build-release/1.0.8/SourcePackages/checkouts/aws-sdk-ios-spm/DependantTargets/AWSConnectParticipant-Target/empty.m
