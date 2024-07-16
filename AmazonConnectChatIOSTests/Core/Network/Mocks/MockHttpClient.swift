dedf6f13f19b51e040c119b5e7f11b5ff8946d97
Michael Liao <mikeliao@amazon.com>
2024-07-16 14:36:59 -0700

    Updating headers + fixing licensing type (#32)

--- AmazonConnectChatIOSTests/Core/Network/Mocks/MockHttpClient.swift
+++ AmazonConnectChatIOSTests/Core/Network/Mocks/MockHttpClient.swift
@@ -1,7 +1,5 @@
-//
-//  MockHttpClient.swift
-//  AmazonConnectChatIOSTests
-//
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 
 import XCTest
 import UniformTypeIdentifiers
